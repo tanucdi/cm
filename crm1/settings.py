2b7015b09dfe9863d9edd56a5f90be7f165b952b
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-07-05 18:46:29 +0530

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tanishk-crm.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '*********'
-EMAIL_HOST_PASSWORD = '*********'
+EMAIL_HOST_USER = 'tanucdi7@gmail.com'
+EMAIL_HOST_PASSWORD = 'gmanishq@7.'
